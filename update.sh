9eedc857b70b225f0ff1e33d24e67ca7e0ed70d3
reiz <robert.reiz.81@gmail.com>
2015-06-23 09:39:35 +0200

    Add example image

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
 VERSIONEYE_SERVER=https://www.versioneye.com
-PROJECT_ID=<YOUR_PROJECT_ID>  # https://www.versioneye.com/user/projects/<PROJECT_ID>
 API_KEY=<YOUR_SECRET_API_KEY> # Get it from here: https://www.versioneye.com/settings/api
+PROJECT_ID=<YOUR_PROJECT_ID>  # https://www.versioneye.com/user/projects/<PROJECT_ID>
 
 
 json=$( curl -F name=project_file -F project_file=@$1 "${VERSIONEYE_SERVER}/api/v2/projects/${PROJECT_ID}?api_key=${API_KEY}" )
