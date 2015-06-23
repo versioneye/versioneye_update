3a90448ef5b6a025bcfbbf2eb75e683308b0f4e1
reiz <robert.reiz.81@gmail.com>
2015-06-23 09:17:18 +0200

    Add update.sh

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+VERSIONEYE_SERVER=https://www.versioneye.com
+PROJECT_ID=<YOUR_PROJECT_ID>  # https://www.versioneye.com/user/projects/<PROJECT_ID>
+API_KEY=<YOUR_SECRET_API_KEY> # Get it from here: https://www.versioneye.com/settings/api
+
+
+json=$( curl -F name=project_file -F project_file=@$1 "${VERSIONEYE_SERVER}/api/v2/projects/${PROJECT_ID}?api_key=${API_KEY}" )
+
+project_id=$(echo $json | jq '.id' | sed 's/"//g' )
+dep_count =$(echo $json | jq '.dep_number')
+out_number=$(echo $json | jq '.out_number')
+violations=$(echo $json | jq '.licenses_red')
+
+echo ""
+echo "Dependencies: $dep_number"
+echo "Outdated: $out_number"
+echo "License violations: $violations"
