88cc7b91ccde8f6ec1f067bf7c90e42b60784251
Robert Reiz <robert.reiz.81@gmail.com>
2016-07-29 15:45:20 +0200

    Add orga_name

--- update.sh
+++ update.sh
@@ -3,9 +3,10 @@
 VERSIONEYE_SERVER=https://www.versioneye.com
 API_KEY=<YOUR_SECRET_API_KEY> # Get it from here: https://www.versioneye.com/settings/api
 PROJECT_ID=<YOUR_PROJECT_ID>  # https://www.versioneye.com/user/projects/<PROJECT_ID>
+ORGA_NAME=<YOUR_ORGANISATION_NAME>
 
 
-json=$( curl -F name=project_file -F project_file=@$1 "${VERSIONEYE_SERVER}/api/v2/projects/${PROJECT_ID}?api_key=${API_KEY}" )
+json=$( curl -F name=project_file -F orga_name=${ORGA_NAME} -F project_file=@$1 "${VERSIONEYE_SERVER}/api/v2/projects/${PROJECT_ID}?api_key=${API_KEY}" )
 
 project_id=$(echo $json | jq '.id' | sed 's/"//g' )
 dep_number=$(echo $json | jq '.dep_number')
